ecb78329b3758b3ef565ad9d2a8894043ad97e66
breyes24 <breyes24@alumnos.uaq.mx.com>
2024-09-14 23:25:53 -0600

    primer commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const morgan=require('morgan');
+const express= require('express');
+const app=express();
+
+const pokemon=require('./routes/pokemon');
+const user=require('./routes/user');
+
+const auth =require('./middleware/auth');
+const notFound=require('./middleware/notFound');
+const index=require ('./middleware/index');
+const cors=require('./middleware/cors');
+
+app.use(cors);
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get("/",index);
+app.use("/usuario",user);
+app.use(auth);
+app.use("/pokemon",pokemon);
+app.use(notFound);
+
+app.listen(process.env.PORT||3000,()=>{
+        console.log("Server is running... ");
+    });
